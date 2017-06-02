4373b937a6baaafb3f29ab81b9a3c864afe3b942
sagethedawg <sagethedawg@gmail.com>
2017-06-02 12:35:39 -0700

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Echo.ioe</title>
+
+<link rel="stylesheet" href="css/custom.css">
+    <style>
+   	 #if-location {
+   	 position: absolute; left: 10px; top:200px;
+   	 width:800px;
+   	 height500;
+    }
+</style>
+
+</head>
+<body>
+<!-- add more menue items here -->
+<a href="phpcode/" class = "menu" >php code</a>&nbsp;
+<a href="downloads/" class = "slink" >Downloads</a>&nbsp;
+<a href="qnb/" class = "menu" >PMA</a>&nbsp;
+<a href="logs/" class = "slink" target = " blank" >Logs</a>
+<a href="dev/" class = "menu" target = " blank">DEV</a>
+<a href="contact.html" class = "slink" target = "if">Contact</a>
+<img src = "img/delta.png" align = "left" height = "28px">
+<pre>
+Echo.ioe
+</pre>
+<iframe id="if-location" name="if" arc="into.html" scrolling="yes" frameborder="3">
+</iframe>
+
+</body>
+</html>
+
